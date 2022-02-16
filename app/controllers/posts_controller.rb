ac9d45c82720407e26ea47827952f9ca6f3f7e58
weillium <will.liao@kellogg.northwestern.edu>
2022-02-16 02:29:52 +0000

    complete connects

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,22 @@
 class PostsController < ApplicationController
+
+    def index
+        @post = Post.all
+    end
+
+    def show
+        @post = Post.find(params["id"])
+    end
+    
+    def new
+        @post = Post.new
+        @post.place_id = params["place_id"]
+    end
+    
+    def create
+        @post = Post.new(params["post"])
+        @post.save
+        redirect_to controller: "places", action: "show", id: @post.place_id
+    end
+
 end
