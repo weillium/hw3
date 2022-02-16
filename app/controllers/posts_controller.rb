a89d4285e9372568169f46d7396bc9be6099575a
weillium <will.liao@kellogg.northwestern.edu>
2022-02-16 02:40:00 +0000

    complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,4 @@
 class PostsController < ApplicationController
-
-    def index
-        @post = Post.all
-    end
-
-    def show
-        @post = Post.find(params["id"])
-    end
     
     def new
         @post = Post.new
